c3d3a53c5f4eb1aaf5f30f73a35bbb690050f090
wangaoming <1622504316@qq.com>
2020-06-17 23:37:27 +0800

    全部权益页面

---
+++ pages/legal/legal.js
@@ -0,0 +1,102 @@
+// pages/legal/legal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+        { 'bgc':'#ffefed',
+          'img':'/images/ledal/app_img_vip_icon_vip_commodity.png',
+          'text1':'VIP会员专属商品',
+          'text2':'专属信纸皮肤主题任意装扮'
+        },
+        { 'bgc':'',
+        'img':'/images/ledal/app_img_vip_icon_suipian.png',
+        'text1':'碎片变现',
+        'text2':'背包碎片轻松变现'
+      },
+
+      { 'bgc':'',
+      'img':'/images/ledal/app_img_vip_icon_shangcheng.png',
+      'text1':'商城特惠',
+      'text2':'杂货铺一年预计可省999元'
+    },
+
+
+    
+
+    ],
+    list1:[
+      { 'bgc':'',
+      'img':'/images/ledal/app_img_vip_icon_join.png',
+      'text1':'社团无限加入',
+      'text2':'社团无限加入，做社交达人'
+    },
+    { 'bgc':'',
+      'img':'/images/ledal/app_img_vip_icon_exclusive.png',
+      'text1':'创建专属社团',
+      'text2':'创建社团无等级门槛'
+    },
+    ]
+
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
