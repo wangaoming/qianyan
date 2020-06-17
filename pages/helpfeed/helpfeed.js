c19952c1812765e2bf8de42cc76bd5c00e191f4b
wangaoming <1622504316@qq.com>
2020-06-17 10:21:04 +0800

    帮助与反馈页面

--- pages/helpfeed/helpfeed.js
+++ pages/helpfeed/helpfeed.js
@@ -1,40 +1,39 @@
-// pages/setting/setting.js
+// pages/helpfeed/helpfeed.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    list:[
+      {
+        'name':'什么是人气值？'
+      },
+      {
+        'name':'怎么获得浅币？'
+      },
+      {
+        'name':'浅币可以用来干嘛？'
+      },
+      {
+        'name':'怎么退出社团/社团聊天室或者群聊？'
+      },
+      {
+        'name':'怎么找到全部社团？如何搜索社团？'
+      },
+      {
+        'name':'有哪些等级？每个等级的权益都有哪些？'
+      },
+      {
+        'name':'怎么设置水印？'
+      },
+      {
+        'name':'怎么开启或关闭手机内的签到提醒、日程？'
+      },
+    ]
 
   },
-hidehandle(){
- wx.navigateTo({
-   url: '../../pages/hide/hide',
-   success: function(res){
-     // success
-   },
-   fail: function() {
-     // fail
-   },
-   complete: function() {
-     // complete
-   }
- })
-},
-helphandle(){
-wx.navigateTo({
-  url: '../../pages/helpfeed/helpfeed',
-  success: function(res){
-    // success
-  },
-  fail: function() {
-    // fail
-  },
-  complete: function() {
-    // complete
-  }
-})
-},
+
   /**
    * 生命周期函数--监听页面加载
    */
