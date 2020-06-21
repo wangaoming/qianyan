54af8121135daf8d1736590594c804c843d0f813
wangaoming <1622504316@qq.com>
2020-06-21 19:24:10 +0800

    社团-话题页面

---
+++ components/topic/topic.js
@@ -0,0 +1,89 @@
+// components/topic/topic.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+    list:[
+{'topic':'我的话题',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747725694&di=a48a821d045e7c66f1b739ee44058435&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D268441563%2C2047262967%26fm%3D214%26gp%3D0.jpg',
+'title':'古风书苑',
+'liulan':'517083',
+'pinglun':'1592',
+'renshu':'6.2万'
+},
+{'topic':'推荐话题',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747725032&di=601a46824a3e7681e7a25236427ebfea&imgtype=0&src=http%3A%2F%2Fwww.espsty.com%2Fuploads%2Fcaiji%2F2019-12-02%2F9253d89b632a55a077bd66c012aaba4f.jpeg',
+'title':'你喜欢的零食是？',
+'liulan':'517083',
+'pinglun':'5678',
+'renshu':'8.90万'
+}
+,
+{'topic':'社团话题',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747725028&di=f0201cd0c3d3b596d632b63a84f06467&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201510%2F31%2F20151031234430_u5Yhj.jpeg',
+'title':'男女分手后会成为朋友么？',
+'liulan':'152',
+'pinglun':'0',
+'renshu':'9'
+}
+,
+{'topic':'情感小屋',
+'img':'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3465769305,2497118094&fm=26&gp=0.jpg',
+'title':'找闺蜜',
+'liulan':'3558992',
+'pinglun':'25743',
+'renshu':'42.58万'
+}
+,
+
+{'topic':'学霸党',
+'img':'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=204268622,4042276298&fm=26&gp=0.jpg',
+'title':'有要交笔友的嘛',
+'liulan':'1349838',
+'pinglun':'11724',
+'renshu':'24.03万'
+}
+,
+{'topic':'碎碎念',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747863523&di=38e3476082707c54aaf64652f320545f&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F14ce36d3d539b6001e970ec5ea50352ac65cb762.jpg',
+'title':'每人说一句谎话吧',
+'liulan':'1876649',
+'pinglun':'10228',
+'renshu':'16.33万'
+}
+,
+{'topic':'二次元',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747863519&di=2797f77ec2edec4433b53c943983132e&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn12%2F96%2Fw2048h2048%2F20180415%2F3a0a-fzcyxmu8334264.jpg',
+'title':'说出你们最喜欢的国漫',
+'liulan':'702285',
+'pinglun':'3067',
+'renshu':'86746'
+}
+,
+{'topic':'时尚娱乐',
+'img':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592747886109&di=90639d63712ff3c5dd30298cd0b77c9c&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190908%2F21%2F1567949094-XFfOtVLxGR.jpg',
+'title':'分享一下你手机里的图片',
+'liulan':'1856166',
+'pinglun':'17878',
+'renshu':'25.06万'
+}
+,
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
